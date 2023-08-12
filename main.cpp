150a025c30fe591efe2362e237a647d25dcbfd4e
Himanshu Kumar <142107937+2024B1531074@users.noreply.github.com>
2023-08-12 21:57:27 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int n;
+cin>>n;
+int b;
+cin>>b;
+int sum=0;
+sum=a+b;
+cout<<" sum of two numbers"<<sum;
+
+    return 0;
+}
